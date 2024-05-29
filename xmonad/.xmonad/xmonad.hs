5927bb730f842052324cdfcd3336e9cfd27f9b1f
talhaahussain <73853725+talhaahussain@users.noreply.github.com>
2024-05-29 15:30:33 +0100

    Fix #6
    Added dmenu config
    Changed xmonad config to reflect current dmenu config
    Current features: centered box, borders, highlighting

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -74,7 +74,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm,               xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_r     ), spawn "dmenu_run -nb '#000000' -sb '#3f3f3f' -l 39")
+    , ((modm,               xK_r     ), spawn "dmenu_run -c -l 20")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
