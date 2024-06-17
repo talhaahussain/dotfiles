e32491127f1d6803b39dbb809290f11ccd77c15b
talhaahussain <73853725+talhaahussain@users.noreply.github.com>
2024-06-17 18:08:11 +0100

    Changed slock init mode to reference black screen
    Likely will not fix #3

--- slock/.slock/config.def.h
+++ slock/.slock/config.def.h
@@ -4,7 +4,7 @@ static const char *group = "nobody";
 
 /* image files can be used if absolute path is given */
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "/opt/DMG.png",     /* after initialization */
+	[INIT] =   "#000000",   /* after initialization */
 	[INPUT] =  "/opt/NAV.png",   /* during input */
 	[FAILED] = "/opt/DMG.png",   /* wrong password */
 };
